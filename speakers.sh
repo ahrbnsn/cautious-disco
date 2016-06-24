e4a67edb118ec9d79b6242389886c74a3ecafdfe
Ashley Robinson <ashleyhowerobinson@gmail.com>
2016-06-23 19:12:16 -0600

    Merged up & down commands into one

--- speakers.sh
+++ speakers.sh
@@ -1,30 +1,34 @@
 #!/bin/bash   
 
+# Controls Media Server audio output
+# 
+#   on)   Opens Airfoil Speakers app & connects
+#   off)  Quits Airfoil Speakers
+#   mute) Mutes speakers
+#   vol)  Sets exact volume
+#   v)    With no argument, tells current volume on remote mac. 
+#         
+#         Options
+#         --------------
+#         +  Increases volume by 20
+#         -  Decreases volume by 20
+#         [integer] Increases or decreases volume by amount specified
+# 
+# 
+# 
 
-
-# Set defaults 
-PONY=""
 
 
 # Load configuration file
 source $HOME/ArrestingDevelopment/scripts/config
 
 speakers() {
-
-  # Commands: on, off, -v [integer 0-100], mute, volume, up, down
-
-  volume=""
   
-  login() {
-    ssh $PONY $1
-
-  }
-
   case "$1" in 
 
     # Turns speakers on and sets volume to medium level
     on) volume="40"
-        login "open -a 'Airfoil Speakers' && osascript -e 'set volume output volume 40'"
+        login "open -a 'Airfoil Speakers' && osascript -e 'set volume output volume 40'" 
         osascript -e "tell application \"Airfoil\"" \
           -e "repeat until speaker \"PonyExpress\" exists" \
           -e "end repeat"\
@@ -33,61 +37,56 @@ speakers() {
         echo "Speakers on"
         ;;
 
+
     # Turns speakers off
     off) volume="0"
          login "osascript -e 'quit app \"Airfoil Speakers\"'"
          echo "Speakers off"
          ;;
 
-    # Sets volume by integer
-    # TODO: Check that $2 is between 0-100
-    -v) volume=$2
-        login "osascript -e 'set volume output volume $2'"
-        echo "Speakers volume $2"
+
+    # Retrieves current volume or tweaks it
+    vol) volume=$2
+        if [[ "$2" -ge "0" && "$2" -le "100" ]]; then
+          login "osascript -e 'set volume output volume $2'"
+          echo "Speakers volume $2"
+        
+        else 
+          echo "Specify a volume between 0-100"
+        fi
         ;;
+
 
     # Mutes Speakers
     mute) volume="0"
           login "osascript -e 'set Volume 0'"
           ;;
 
-    # Tells the current volume 
-    volume) login "osascript -e 'output volume of (get volume settings)'"
-            ;;
 
-    # Turns volume up by 20
-    # TODO: accept a third value to tinker with increments
-    up) volume=$(login "osascript -e 'output volume of (get volume settings)'")
-        volume=$((volume+20));
+    # Turns volume up or down incrementally; with no arguments passed tells current volume
+    v) volume=$(login "osascript -e 'output volume of (get volume settings)'")
+          
+          if [[ -z "$2" ]]; then
+            echo $volume
+            return;
+          
+          elif [[ "$2" = "+" ]] ; then
+           increment="20"
+          
+          elif [[ "$2" = "-" ]] ; then 
+            increment="-20"
+          
+          else
+            increment="$2"
+          fi
 
-        if [ "$volume" -lt "100" ]; then
+
+          volume=$((volume+increment));
+          echo $volume
           login "osascript -e 'set volume output volume $volume'"
           echo "Volume set at $volume"
+          ;;          
 
-        else
-          volume="100"
-          login "osascript -e 'set volume output volume 100'"
-          echo "Volume maxed"
-
-        fi
-        ;;
-
-    # Turns volume down by 20
-    # Should also accept a third argument for incremental adjustments
-    down) volume=$(login "osascript -e 'output volume of (get volume settings)'")
-          volume=$((volume-20));
-
-          if [ "$volume" -gt "0" ]; then
-            login "osascript -e 'set volume output volume $volume'"
-            echo "Volume set at $volume"
-
-          else
-            volume="0"
-            login "osascript -e 'set volume output volume 0'"
-            echo "Muted"
-
-          fi
-          ;;
   esac    
 
 }
